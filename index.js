775caebe915598fbd019685db4e5e91ac1f7c825
sunqimin <sunqimin@xiaomi.com>
2016-05-28 14:17:49 +0800

    update menu function

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // @grant           GM_addStyle
 // @grant           GM_setValue
 // @grant           GM_getValue
+// @grant           GM_registerMenuCommand
 // @grant           GM_log
 // @run-at          document-end
 // ==/UserScript==
@@ -15,50 +16,99 @@
     'use strict';
     var log = GM_log;
     var slideRows = GM_getValue("slideRows") || {};
-    console.log(slideRows, "slideRows")
-        //    $(".i_menu_community_msg_btn .num").remove();
-    var headstyle = ".i_menu_community_msg_btn .num" +
-        "{display:none !important;opacity:0 !important;} " +
-        ".slide-btn" +
-        "{margin-top:5px;font-size: 10px;padding: 2px;position: absolute;left: -32px;top: 6px;margin: auto;}" +
-        ".container-row .b-l .b-body" +
-        "{margin-bottom:20px;}"+
-        "div.container-row .b-r.b-hotspot"+
-        "{height:initial !important;min-height:initial !important;}";
-    GM_addStyle(headstyle);
-    $("#index_container>.container-row .b-l .b-head-i+.b-head-t").after('<button class="slide-btn">收起</button>');
-    var id;
-    var $row;
-    var $btn;
-    var state;
-    for(var i in slideRows){
-        id=i;
-        $row=$("#index_container>.container-row#"+id);
-        $btn=$row.find(".slide-btn");
-        $row.data("up","0");
-        state=false;
-        setState($btn,$row,state);
-    }
-    function setState($btn, $row, state) {
-        if (!state) {
-            $row.data("up", "0");
-            $btn.text("放下");
-            $row.find(".b-body").slideUp();
-            slideRows[$row.attr("id")] = 1;
-            GM_setValue("slideRows", slideRows)
-            $row.find(".b-r").css("min-height", "0px").css("height", "0px");
-        } else {
-            $row.data("up", "1");
-            $btn.text("收起");
-            $row.find(".b-body").slideDown();
-            delete slideRows[$row.attr("id")];
-            GM_setValue("slideRows", slideRows)
+    var initStyle = "";
+    var defaultOpen=true;
+    var defaultClost=true;
+    function register(doTip, cancelTip,defaultState, name, onStart, onChange) {
+        if(GM_getValue(name)==undefined){
+            GM_setValue(name,defaultState)
         }
+        if (GM_getValue(name)) {
+            var result = onStart();
+            if (result.style) {
+                initStyle += result.style;
+            }
+            GM_registerMenuCommand(cancelTip, function () {
+                GM_setValue(name, false);
+                onChange(false);
+            });
+        } else {
+            GM_registerMenuCommand(doTip, function () {
+                GM_setValue(name, true);
+                onChange(true);
+            });
+        }
+
     }
-    $(".slide-btn").on("click", function () {
-        var $btn = $(this);
-        var $row = $btn.parents(".container-row");
-        var state = !($row.data("up")=="1"||!$row.data("up"));
-        setState($btn,$row,state);
+
+
+//    GM_registerMenuCommand("Hello, world (simple)", helloSimple);
+
+    register("隐藏消息红点", "显示消息红点",defaultOpen, "hideMsg", function () {
+        var style = ".i_menu_community_msg_btn .num" +
+            "{display:none !important;opacity:0 !important;} ";
+        return {
+            style: style
+        }
+    }, function () {
+        location.reload();
     });
+
+    register("开启折叠功能", "关闭折叠功能",defaultOpen, "slide", function () {
+        var style = ".slide-btn" +
+            "{margin-top:5px;font-size: 10px;padding: 2px;position: absolute;left: -32px;top: 6px;margin: auto;}" +
+            ".container-row .b-l .b-body" +
+            "{margin-bottom:20px;}" +
+            "div.container-row .b-r.b-hotspot" +
+            "{height:initial !important;min-height:initial !important;}";
+
+        $("#index_container>.container-row .b-l .b-head-i+.b-head-t").after('<button class="slide-btn">收起</button>');
+
+        function helloSimple() {
+            log("hellol");
+        }
+
+        var id;
+        var $row;
+        var $btn;
+        var state;
+        for (var i in slideRows) {
+            id = i;
+            $row = $("#index_container>.container-row#" + id);
+            $btn = $row.find(".slide-btn");
+            $row.data("up", "0");
+            state = false;
+            setState($btn, $row, state);
+        }
+
+        function setState($btn, $row, state) {
+            if (!state) {
+                $row.data("up", "0");
+                $btn.text("放下");
+                $row.find(".b-body").slideUp();
+                slideRows[$row.attr("id")] = 1;
+                GM_setValue("slideRows", slideRows)
+                $row.find(".b-r").css("min-height", "0px").css("height", "0px");
+            } else {
+                $row.data("up", "1");
+                $btn.text("收起");
+                $row.find(".b-body").slideDown();
+                delete slideRows[$row.attr("id")];
+                GM_setValue("slideRows", slideRows)
+            }
+        }
+        $(".slide-btn").on("click", function () {
+            var $btn = $(this);
+            var $row = $btn.parents(".container-row");
+            var state = !($row.data("up") == "1" || !$row.data("up"));
+            setState($btn, $row, state);
+        });
+        return {
+            style: style
+        }
+    }, function () {
+        location.reload();
+    });
+    GM_addStyle(initStyle);
+
 })();
